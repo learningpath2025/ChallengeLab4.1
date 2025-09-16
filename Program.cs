b1b1b35f41b0d9b0260982f761bd884f188971b0
learningpath2025 <learningpath2025@outlook.com>
2025-09-15 20:49:09 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ChallengeLab4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IfNumberContains3());
+        }
+
+        public static bool IfNumberContains3()
+        {
+            int i = GetPositiveIntegerInput();
+            string si = i.ToString();
+            bool contains3 = false;
+            if(i != -1)
+            {
+                foreach (char c in si)
+                {
+                    if (c == '3')
+                    {
+                        contains3 = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.Write($"Invalid input!");
+            }
+            return contains3;
+        }
+
+        public static int GetPositiveIntegerInput()
+        {
+            Console.Write($"Enter a positive integer number: ");
+            object input = Console.ReadLine() ?? "0" ;
+            int i = Convert.ToInt32(input);
+
+            if (IsInteger(input) && IsPositive(i))
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static bool IsInteger(object input)
+        {
+            int i;
+            if (int.TryParse(input.ToString(), out i))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsPositive(int input)
+        {
+            if (input > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ChallengeLab4.1
+﻿# ChallengeLab4.1
+
+- If number contains 3
+- Write a method that checks if given number (positive integer) contains digit 3. 
+- Do not convert number to other type. Do not use built-in functions like Contains(), StartsWith(), etc.
+- Expected input and output
+	- IfNumberContains3(7201432) → true 
+	- IfNumberContains3(87501) → false
